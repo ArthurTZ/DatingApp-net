40061bb4d3256849bc31bd1c771c3a68829ca0e8
Pedro Arthur <pedroarthurzpyz@outlook.com>
2024-11-04 11:38:12 -0300

    feat: adicao de certificado ssl angular

--- API/Program.cs
+++ API/Program.cs
@@ -8,11 +8,19 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt =>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddCors(option => option.AddDefaultPolicy(policy =>
+{
+    policy.AllowAnyOrigin();
+    policy.AllowAnyMethod();
+    policy.AllowAnyHeader();
+    policy.WithOrigins("http://localhost:4200", "https://localhost:4200");
+}));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
 app.MapControllers();
+app.UseCors();
 
 app.Run();
